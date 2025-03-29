fe3f9f6ec1b1018992238efed5525165c6ab1929
at189 <ab8613312@gmail.com>
2025-03-29 12:01:25 +0800

    class4/class4.py

--- class4/class4.py
+++ class4/class4.py
@@ -30,21 +30,20 @@
 # for回圈變數 in 範圍:
 # 迴圈變數只能活在for回圈中
 # 迴圈變數每回合都會從範圍中取出一個值
-for i in range(10):  # range(10)的範圍是0到9
-    print(i)
-for i in range(1, 6):  # range=1到5
-    print(i)
+# for i in range(10):  # range(10)的範圍是0到9
+#     print(i)
+# for i in range(1, 6):  # range=1到5
+#     print(i)
 
-for i in range(1, 6, 2):  # range=1,3,5
-    print(i)
+# for i in range(1, 6, 2):  # range=1,3,5
+#     print(i)
 
-import turtle as t
+# import turtle as t
 
-t.speed(0)  # 設定速度
-for i in range(4):
-    t.forward(150)  # 前進100步
-    t.right(90)  # 右邊向前轉90度
-    t.done()  # 讓視窗不要關閉
+# for i in range(4):
+#     t.forward(150)  # 前進100步
+#     t.right(90)  # 右邊向前轉90度
+# t.done()  # 讓視窗不要關閉
 
 import turtle
 
@@ -52,3 +51,4 @@ turtle.color("red")  # 設定顏色
 turtle.shape("turtle")  # 設定形狀
 turtle.stamp()  # 蓋章
 turtle.penup()  # 提筆
+turtle.done()  # 讓視窗不要關閉
