0a08f2e8f393619e2a85082d5087449a3aa087cd
at189 <ab8613312@gmail.com>
2025-03-29 10:35:02 +0800

    class3/hw.py

--- class3/hw.py
+++ class3/hw.py
@@ -16,11 +16,32 @@
 
 def fahrenheit_to_celsius():
     try:
-        fahrenheit = float(input("請輸入華氏溫度: "))
+        f = float(input("請輸入華氏溫度: "))
         celsius = (fahrenheit - 32) * 5 / 9
         print(f"華氏溫度{fahrenheit}F等於攝氏溫度{celsius}C")
-    except ValueError:
+    except:
         print("輸入錯誤!")
 
 
-fahrenheit_to_celsius()
+def grade_judgment(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "E"
+
+
+score = int(input("請輸入成績："))
+print(f"您的等第為：{grade_judgment(score)}")
+
+try:
+    f = float(input("請輸入華氏溫度:"))
+    c = (fahrenheit - 32) * 5 / 9
+    print(f"華氏溫度{fahrenheit}F等於攝氏溫度{celsius}C")
+except:
+    print("輸入錯誤!")
