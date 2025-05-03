838bb9fc92e861e8d43f8390b761f291cf50c28c
at189 <ab8613312@gmail.com>
2025-05-03 10:44:06 +0800

    class.7/hw.py

--- class.7/hw.py
+++ class.7/hw.py
@@ -18,6 +18,12 @@ EX:
 
 f = int(input("請輸入開始整數"))
 a = int(input("請輸入結束整數"))
-for i in range(f, a):
-    if i % 3 == 0:
-        print(i)
+for n in range(f, a + 1):
+    if n > 1:
+        ans = "是質數"
+        for i in range(2, n):
+            if n % i == 0:
+                ans = "不是質數"
+
+        if ans == "是質數":
+            print(n)
